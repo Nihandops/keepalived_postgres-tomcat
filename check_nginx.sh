a08ab78b38c732c3cf6f1d22e935c32fdc68995e
Nihan <128370614+Nihandops@users.noreply.github.com>
2025-08-04 15:49:18 +0600

    Create check_nginx.sh

---
+++ check_nginx.sh
@@ -0,0 +1,1 @@
+## nginx we don't need any script for nginx because nginx health check can do by it's parameter which can detect by keepalived, we add the nginx health check script in keepalived.conf
